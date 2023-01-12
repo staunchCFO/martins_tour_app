604d6f86e7160e423783f6328a6da7384ae1981e
Emeka Okezie <okezieemeka949@gmail.com>
2023-01-12 22:03:30 +0100

    Completed my project

---
+++ explore.js
@@ -0,0 +1,30 @@
+
+
+
+function openpage(){
+    var x = document.getElementById("searchbar").value;
+    if(x == "nike art gallery"){
+        window.open("tour.html")
+    }
+    if(x == "lekki conservation centre"){
+        window.open("tour.html")
+    }
+    if(x == "ikeja city mall"){
+        window.open("tour.html")
+    }
+    if(x == "leisure mall"){
+        window.open("tour.html")
+    }
+    if(x == "freedom park"){
+        window.open("tour.html")
+    }
+    if(x == "tarkwa bay beach"){
+        window.open("tour.html")
+    }
+    if(x == "oniru beach"){
+        window.open("tour.html")
+    }
+    if(x = "national museum"){
+        window.open("tour.html")
+    }
+}
